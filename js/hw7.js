27389dd5f5472eed52d1dd255edb3a679af75509
Yevgeniy-M <jhorg.htk@gmail.com>
2020-11-14 18:05:51 +0200

    Основа

---
+++ js/hw7.js
@@ -0,0 +1,21 @@
+const targetBtnRef = document.querySelector('.js-target-btn');
+const addListenerBtnRef = document.querySelector('.js-add-listener');
+const removeListenerBtnRRef = document.querySelector('.js-remove-listener');
+
+// const doStuffOnClick = () => {
+//     console.log('Clicked!!!');
+// };
+
+// addListenerBtnRef.addEventListener('click', () => {
+//     targetBtnRef.addEventListener('click', doStuffOnClick);
+// });
+
+// removeListenerBtnRRef.addEventListener('click', () => {
+//     targetBtnRef.removeEventListener('click', doStuffOnClick);
+// });
+
+// // targetBtnRef.addEventListener('click', () => {
+// //     console.log(Date.now());
+// // });
+
+targetBtnRef.addEventListener ('click', )
